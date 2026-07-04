a694f9b88f48cc5d7cccf2cd8a3c52f9ccb058d4
Colin Nguyen <nguyen_c9@denison.edu>
2026-07-04 17:23:29 -0400

    Comment for public and private classes

--- pp.py
+++ pp.py
@@ -1,5 +1,8 @@
 # there are no such things as private or public classes 
 # in python, these are just examples
+# the _ before the name is just to show that the
+# methods are supposed to be private (that's the best we can do)
+# and yes, it's a convention
 class _Private:
     def __init__(self, name):
         self.name = name
